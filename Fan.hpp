be3999fedfa7881cafc8538ce39c6e917a0761a5
jakjan95 <jakubjanusz95@gmail.com>
2020-10-12 20:04:16 +0200

    Refactor in fan class, refactor in LcdDisplay

--- Fan.hpp
+++ Fan.hpp
@@ -1,15 +1,10 @@
 #pragma once
 
 class Fan {
+private:
     int rpm = 0;
 
 public:
-    Fan();
-    Fan(const Fan&);
-    Fan(Fan&&);
-
     void setSpeed(int newRpm);
-    int getSpeed();
-    bool disable();
-    bool enable();
+    int getSpeed() const;
 };
